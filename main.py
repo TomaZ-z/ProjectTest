dfcd674e02ce27c8f5b78c59479ad7412429244e
TomaZ-z <thomasclchiu@gmail.com>
2025-11-29 19:47:01 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,55 @@
+#-------------import-------------#
+
 import random
 import time
 
+#--------------------------------#
+#-----------attributes-----------#
+
+BLACK = "\033[0;30m"
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+BROWN = "\033[0;33m"
+BLUE = "\033[0;34m"
+PURPLE = "\033[0;35m"
+CYAN = "\033[0;36m"
+LIGHT_GRAY = "\033[0;37m"
+DARK_GRAY = "\033[1;30m"
+LIGHT_RED = "\033[1;31m"
+LIGHT_GREEN = "\033[1;32m"
+YELLOW = "\033[1;33m"
+LIGHT_BLUE = "\033[1;34m"
+LIGHT_PURPLE = "\033[1;35m"
+LIGHT_CYAN = "\033[1;36m"
+LIGHT_WHITE = "\033[1;37m"
+BOLD = "\033[1m"
+FAINT = "\033[2m"
+ITALIC = "\033[3m"
+UNDERLINE = "\033[4m"
+BLINK = "\033[5m"
+NEGATIVE = "\033[7m"
+CROSSED = "\033[9m"
+END = "\033[0m"
+
+#--------------------------------#
+#------------variable------------#
 
 gameRun = True
 
+#--------------------------------#
+#-------------events-------------#
 
+def e_basic():
+    print(f"{ITALIC}This is a basic encounter{END}")
 
+def e_slight():
+    print(f"{CROSSED}This is a slightly basic encounter{END}")
+    
+def e_advanced():
+    print(f"{UNDERLINE}This is an advanced encounter{END}")
 
-
+#--------------------------------#
+#------------function------------#
 
 def gameStart ():
     global gameRun
@@ -19,19 +61,6 @@ def gameEnd():
     time.sleep(5)
     gameRun = False
 
-#-------------events-------------#
-
-def e_basic():
-    print("This is a basic encounter")
-
-def e_slight():
-    print("This is a slightly basic encounter")
-    
-def e_advanced():
-    print("This is an advanced encounter")
-
-#--------------------------------#
-
 def event_perc():
     global events
     global et_random
@@ -41,14 +70,8 @@ def event_perc():
     et_chosen = et_random[0]()
     return
 
-
-def event_occurr():
-    print(et_chosen)
-
-
-
-
-
+#--------------------------------#
+#--------------main--------------#
 
 print("Welcome to ProjectTest. A text-based rougelike project being worked on by a idiot!")
 
@@ -67,4 +90,6 @@ while True:
 
 while gameRun is True:
     event_perc()
-    input("next?")
+    input("reroll another")
+
+#--------------------------------#
